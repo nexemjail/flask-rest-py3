72e8cdd0fd10e080cde6e93f764b5ed0d3bfb36d
nexemjail <nexemjail@gmail.com>
2017-01-13 12:43:57 +0300

    Some more tests

--- app/tests.py
+++ app/tests.py
@@ -1,8 +1,10 @@
 import unittest
 
 import json
+from wsgiref.headers import Headers
 
 from app import get_app
+from app.models import User
 from app.utils import ResponseCodes
 
 
@@ -66,6 +68,50 @@ class FlaskTests(unittest.TestCase):
             response = self.get_auth_response()
             self.assertEqual(response.status_code, ResponseCodes.OK)
             self.assertTrue('token' in json.loads(str(response.data, encoding='utf8')))
+
+    def test_get_info(self):
+        with self.app.test_client() as c:
+
+            response = c.post(self.REGISTER_URL, data=json.dumps(self.USER_PAYLOAD), **self.JSON_CONTENT_TYPE)
+            user = json.loads(str(response.data, encoding='utf-8')).get('data')
+            user_id = user.get('id')
+
+            response = self.get_auth_response()
+            token = json.loads(str(response.data, encoding='utf-8')).get('token')
+
+            h = Headers()
+            h.add_header('Authorization', self.JWT_KEY + token)
+
+            response = c.get('/{}/'.format(user_id), headers=h)
+
+            response_data = json.loads(str(response.data))
+
+            self.assertEqual(response_data['username'], self.USER_PAYLOAD['username'])
+
+    # def test_invalid_token(self):
+    #     self.client.post('/register/', self.sample_user_payload)
+    #     response = self._get_auth_response()
+    #     token = response.data['token']
+    #
+    #     user_id = User.objects.get(username=self.sample_user_payload['username']).id
+    #
+    #     response = self.client.get('/{}/'.format(user_id), HTTP_AUTHORIZATION=self.JWT_KEY + token + 'seed')
+    #
+    #     self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
+    #
+    # def test_expiration(self):
+    #
+    #     now = datetime.now()
+    #     self.client.post('/register/', self.sample_user_payload)
+    #     response = self._get_auth_response()
+    #     token = response.data['token']
+    #
+    #     with freeze_time(now + timedelta(seconds=320)):
+    #         user_id = User.objects.get(username=self.sample_user_payload['username']).id
+    #
+    #         response = self.client.get('/{}/'.format(user_id), HTTP_AUTHORIZATION=self.JWT_KEY + token)
+    #
+    #         self.assertEqual(response.status_code, 403)
 
 
 if __name__ == '__main__':
