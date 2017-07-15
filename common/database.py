78586318e77971d242a69df9d1820dda1c07ff8e
nexemjail <nexemjail@gmail.com>
2017-07-15 23:18:59 +0300

    Added pytest pep8

--- common/database.py
+++ common/database.py
@@ -1,11 +1,8 @@
-from abc import ABCMeta
 from sqlalchemy import Column, Integer
 from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.declarative.api import DeclarativeMeta
 from sqlalchemy.orm.scoping import scoped_session
 from sqlalchemy.orm.session import sessionmaker
-from sqlalchemy.sql.schema import MetaData
 
 from common import app
 
@@ -29,4 +26,3 @@ decl_base.query = db_session.query_property()
 class Base(decl_base):
     __abstract__ = True
     id = Column(Integer, primary_key=True, autoincrement=True)
-
